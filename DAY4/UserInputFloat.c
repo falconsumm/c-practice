7a46925286628dbd769adb268e920ff92f3c98ee
Abhishek Yadav <falconfalls27@gmail.com>
2026-01-23 20:58:14 +0530

    input fload

---
+++ DAY4/UserInputFloat.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    float value;
+    printf("Enter a floating-point number: ");
+    scanf("%f", &value);
+    printf("You entered: %.2f", value);
+    return 0;
+}
